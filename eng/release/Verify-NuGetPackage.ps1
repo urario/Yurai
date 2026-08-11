ea027a63972c3eef1e52c421eb7d2c9bec471fdf
urario <yutayuta423@gmail.com>
2026-08-11 10:48:42 +0900

    fix: address release pipeline review comments

--- eng/release/Verify-NuGetPackage.ps1
+++ eng/release/Verify-NuGetPackage.ps1
@@ -45,7 +45,7 @@ try {
     Assert-Condition ($metadata.id -eq $ExpectedPackageId) "Package ID '$($metadata.id)' does not match '$ExpectedPackageId'."
     Assert-Condition ($metadata.version -eq $ExpectedVersion) "Package version '$($metadata.version)' does not match '$ExpectedVersion'."
     Assert-Condition ($metadata.license.type -eq 'expression' -and $metadata.license.'#text' -eq 'MIT') 'Package license must be the MIT expression.'
-    Assert-Condition (($metadata.dependencies.dependency | Measure-Object).Count -eq 0) 'The package contains runtime dependencies.'
+    Assert-Condition (($metadata.dependencies.SelectNodes('.//*[local-name()="dependency"]') | Measure-Object).Count -eq 0) 'The package contains runtime dependencies.'
 
     $archive = [IO.Compression.ZipFile]::OpenRead($PackagePath)
     try {
@@ -54,13 +54,48 @@ try {
         Assert-Condition ($entryNames -contains 'README.md') 'README.md is not included in the package.'
         Assert-Condition ($entryNames -contains 'LICENSE') 'LICENSE is not included in the package.'
 
-        $libraryEntries = @($entryNames | Where-Object { $_ -like 'lib/*' })
-        Assert-Condition ($libraryEntries.Count -gt 0) 'The package contains no library assembly.'
-        Assert-Condition (-not ($libraryEntries | Where-Object { $_ -notlike 'lib/netstandard2.0/*' })) 'The package contains an unexpected library target.'
+        Assert-Condition ($entryNames -contains 'lib/netstandard2.0/Yurai.dll') 'The package does not contain lib/netstandard2.0/Yurai.dll.'
+        Assert-Condition ($entryNames -contains 'lib/netstandard2.0/Yurai.xml') 'The package does not contain XML documentation.'
+        Assert-Condition (-not ($entryNames | Where-Object { $_ -like 'lib/*' -and $_ -notlike 'lib/netstandard2.0/*' })) 'The package contains an unexpected library target.'
         Assert-Condition (-not ($entryNames | Where-Object { $_ -like 'runtimes/*' })) 'The package contains runtime-specific assets.'
     }
     finally {
         $archive.Dispose()
+    }
+
+    $symbolsArchive = [IO.Compression.ZipFile]::OpenRead($SymbolsPackagePath)
+    try {
+        $symbolsNuspecEntry = $symbolsArchive.GetEntry($nuspecPath.Name)
+        Assert-Condition ($null -ne $symbolsNuspecEntry) 'The symbols package does not contain the matching nuspec file.'
+        $symbolsReader = New-Object IO.StreamReader($symbolsNuspecEntry.Open())
+        try {
+            $symbolsNuspec = [xml]$symbolsReader.ReadToEnd()
+        }
+        finally {
+            $symbolsReader.Dispose()
+        }
+
+        $symbolsMetadata = $symbolsNuspec.package.metadata
+        Assert-Condition ($symbolsMetadata.id -eq $ExpectedPackageId) 'The symbols package ID does not match the main package.'
+        Assert-Condition ($symbolsMetadata.version -eq $ExpectedVersion) 'The symbols package version does not match the main package.'
+        Assert-Condition ($null -ne ($symbolsMetadata.packageTypes.packageType | Where-Object { $_.name -eq 'SymbolsPackage' })) 'The symbols package type is missing.'
+        Assert-Condition (($symbolsMetadata.dependencies.SelectNodes('.//*[local-name()="dependency"]') | Measure-Object).Count -eq 0) 'The symbols package contains dependencies.'
+
+        $pdbEntry = $symbolsArchive.GetEntry('lib/netstandard2.0/Yurai.pdb')
+        Assert-Condition ($null -ne $pdbEntry) 'The symbols package does not contain lib/netstandard2.0/Yurai.pdb.'
+        $pdbStream = $pdbEntry.Open()
+        try {
+            $pdbBytes = New-Object IO.MemoryStream
+            $pdbStream.CopyTo($pdbBytes)
+            $pdbText = [Text.Encoding]::ASCII.GetString($pdbBytes.ToArray())
+            Assert-Condition ($pdbText -match 'raw\.githubusercontent\.com/urario/Yurai/') 'The PDB does not contain SourceLink information for urario/Yurai.'
+        }
+        finally {
+            $pdbStream.Dispose()
+        }
+    }
+    finally {
+        $symbolsArchive.Dispose()
     }
 
     Write-Output "Verified $ExpectedPackageId ${ExpectedVersion}: runtime dependencies, metadata, and package contents."
